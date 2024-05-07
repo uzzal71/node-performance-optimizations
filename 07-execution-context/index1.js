854ab94f91a4b0075b922b5ade8946004a0d3e2f
uzzal71 <uzzalroy.acm@gmail.com>
2024-05-07 11:22:58 +0600

    execution context

---
+++ 07-execution-context/index1.js
@@ -0,0 +1,29 @@
+/**
+ * Step --> 1
+ Tokenization
+ ++++++++++++
+ function --> keyword
+ sayName --> function name
+ () { --> Defining function
+ var ---> keyword
+ name --> variable name
+ "someName" --> variable value string
+
+ Step --> 2
+ AST(Avstract Syntax Tree)
+
+ Step --> 3
+ Machine Code
+
+ Step --> 4
+ If JavaScript Code Then Run Browser
+ else Node.js Then Run Node.js Run Time Environment
+
+ step --> 5
+ Lexical Environment
+ */
+
+function sayName() {
+  var name = "someName";
+  console.log("The name is " + name);
+}
