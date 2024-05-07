2537126fbbcb5ce23cf1c494405e4478be46faf0
uzzal71 <uzzalroy.acm@gmail.com>
2024-05-07 13:36:15 +0600

    execution context

---
+++ 08-hoisting/02-function.js
@@ -0,0 +1,14 @@
+// invoke a function, chase()
+chase();
+
+// declare a function, chase()
+function chase() {
+  console.log("Tom chases Jerry!");
+  // invoke a function, caught()
+  caught();
+}
+
+// Declare a function, caught()
+function caught() {
+  console.log("Tom caught Jerry :(");
+}
