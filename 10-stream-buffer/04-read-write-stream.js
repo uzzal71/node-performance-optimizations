beb8c7c610864b3e64ddcdf66a39957d51fe020a
uzzal71 <uzzalroy.acm@gmail.com>
2024-05-08 12:35:08 +0600

    stream

---
+++ 10-stream-buffer/04-read-write-stream.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+
+ourReadStream.on("data", (chunk) => {
+  ourWriteStream.write(chunk.toString());
+});
+
+console.log("hello");
