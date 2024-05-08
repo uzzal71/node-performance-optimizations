0da295a099f637face7e72c1ef5e290c60a23d5c
uzzal71 <uzzalroy.acm@gmail.com>
2024-05-08 13:00:31 +0600

    stream

---
+++ 11-core-module'/event.js
@@ -0,0 +1,7 @@
+var events = require("events");
+var eventEmitter = new events.EventEmitter();
+
+eventEmitter.on("scream", function () {
+  console.log("A scream is detected!");
+});
+eventEmitter.emit("scream");
