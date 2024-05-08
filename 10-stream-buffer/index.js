0163f7a9016deb032957832f37a031c2b752cfd5
uzzal71 <uzzalroy.acm@gmail.com>
2024-05-08 12:13:03 +0600

    stream

---
+++ 10-stream-buffer/index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
+ourReadStream.on("data", (chunk) => {
+  console.log(chunk);
+});
+
+console.log("hello");
